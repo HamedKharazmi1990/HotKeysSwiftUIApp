d071228baafa90b2e2a6856199e9c5e8aa72befd
hamed kharazmi <hamed.kharazmi@bandpey.com>
2025-12-28 18:11:36 +0100

    Added main hotkeys view

---
+++ HotKeys/View/MainHotkeysView.swift
@@ -0,0 +1,31 @@
+//
+//  MainHotkeysView.swift
+//  HotKeys
+//
+//  Created by Hamed Kharazmi on 28.12.25.
+//
+
+import SwiftUI
+
+struct MainHotkeysView: View {
+    @State private var searchQuery = ""
+    var vm: HotkeyCategoryViewModel = HotkeyCategoryViewModel()
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                // Sections for each category
+                List(vm.hotkeyCategoryModels) { hotkeyCategory in
+                    // TODO: Implement section view
+                    Text( hotkeyCategory.name)
+                }
+            }
+            .navigationTitle("Xcode Hotkeys")
+            .searchable(text: $searchQuery, prompt: "Search...")
+        }
+    }
+}
+
+#Preview {
+    MainHotkeysView()
+}

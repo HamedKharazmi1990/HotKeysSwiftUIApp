db60bc763b56ebd7b080ac7584d98eb45bfff21a
hamed kharazmi <hamed.kharazmi@bandpey.com>
2025-12-28 13:52:10 +0100

    Initial Commit

---
+++ HotKeys/HotKeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotKeysApp.swift
+//  HotKeys
+//
+//  Created by Hamed Kharazmi on 28.12.25.
+//
+
+import SwiftUI
+
+@main
+struct HotKeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
